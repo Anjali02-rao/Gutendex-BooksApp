8e3f7743f3eeb3d6e60c50769a493981f52001b1
anjali <anjali.kodehode@gmail.com>
2025-02-05 14:09:57 +0100

    final commit

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -6,12 +6,13 @@ export default function BookCard({ book }) {
   return (
     <div className="book-card">
       <Link to={`/Gutendex-BooksApp/book/${book.id}`}>
-        <img
-          src={book.formats["image/jpeg"] || "default-bookimg.jpg"}
-          alt={book.title}
-          className="book-card__image"
-        />
-
+        <div className="book-card__image-container">
+          <img
+            src={book.formats["image/jpeg"] || "default-bookimg.jpg"}
+            alt={book.title}
+            className="book-card__image"
+          />
+        </div>
         <div className="book-card__details">
           <h3 className="book-card__title">{book.title}</h3>
           <p className="book-card__author">
@@ -19,9 +20,6 @@ export default function BookCard({ book }) {
           </p>
           <p className="book-card__language">
             Language: {book.languages?.join(", ") || "N/A"}
-          </p>
-          <p className="book-card__subject">
-            Subject: {book.subjects?.join(", ") || "N/A"}
           </p>
         </div>
       </Link>
